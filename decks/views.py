cfcf0f4bf949d3d2e7d20e9d441b709309fac8b1
Denis Radu <mariodenisradu1705@gmail.com>
2025-02-27 17:48:23 +0100

    login, register, logout

--- decks/views.py
+++ decks/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def login_view(request):
+    if request.method == "POST":
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    login(request, user)
+                    return redirect("index")
+    else:
+        form = AuthenticationForm()
+    return render(request, "auth_form.html", {"form_type": "login", "form": form})
+
+def register_view(request):
+    if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect("index")
+    else:
+        form = UserCreationForm()
+    return render(request, "auth_form.html", {"form_type": "register", "form": form})
+
+def log_out(request):
+    logout(request)
+    return redirect("login")
--- magic_decks/urls.py
+++ magic_decks/urls.py
@@ -21,4 +21,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.log_out, name='logout'),
 ]
